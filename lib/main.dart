98027aeb5694d9acb390bedddb33ba4147efe31e
Paulo Bruno <paulo.bf111@gmail.com>
2020-07-20 13:51:30 -0300

    SliverList added and main.dart update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:Learn_1/routes/AppStructureAndNavigation/Sliver/sliverLayoutBuilder.dart';
+import 'package:Learn_1/routes/AppStructureAndNavigation/Sliver/sliverList.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
           appBarTheme:
               AppBarTheme(iconTheme: IconThemeData(color: Colors.white))),
       title: _title,
-      home: SliverLayoutBuilderPage(),
+      home: SliverListPage(),
     );
   }
 }
---
+++ lib/routes/AppStructureAndNavigation/Sliver/sliverList.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class SliverListPage extends StatefulWidget {
+  @override
+  _SliverListState createState() => _SliverListState();
+}
+
+class _SliverListState extends State<SliverListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: <Widget>[
+          SliverAppBar(
+            centerTitle: true,
+            expandedHeight: 200,
+            pinned: true,
+            floating: false,
+            snap: false,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text(
+                'Sliver List',
+                textAlign: TextAlign.center,
+              ),
+              centerTitle: true,
+            ),
+            backgroundColor: Colors.blue[900],
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate((context, index) {
+              return Container(
+                height: (index % 2 == 0) ? 200 : 400,
+                color: Colors.blue[100 * (index % 2) + 200],
+              );
+            }, childCount: 20),
+          ),
+        ],
+      ),
+    );
+  }
+}
