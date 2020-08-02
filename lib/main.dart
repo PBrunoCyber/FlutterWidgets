c3c34f86456d558e778dc946cecc29c4652d6490
Paulo Bruno <paulo.bf111@gmail.com>
2020-08-02 14:38:35 -0300

    FadeTrasition added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:Learn_1/pages/AnimationAndMotion/decoratatedBoxTransition.dart';
+import 'package:Learn_1/pages/AnimationAndMotion/fadeTransition.dart';
 
 void main() {
   runApp(MyApp());
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         appBarTheme: AppBarTheme(iconTheme: IconThemeData(color: Colors.white)),
       ),
       title: _title,
-      home: DecoratedBoxTransitionPage(),
+      home: FadeTransitionPage(),
     );
   }
 }
---
+++ lib/pages/AnimationAndMotion/fadeTransition.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class FadeTransitionPage extends StatefulWidget {
+  @override
+  createState() => _FadeTransitionState();
+}
+
+class _FadeTransitionState extends State<FadeTransitionPage>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation<double> _animation;
+  bool _isForward = true;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    _animation = Tween<double>(begin: 0, end: 1)
+        .animate(CurvedAnimation(parent: _controller, curve: Curves.ease));
+    _controller.forward();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: AppBar(
+          title: Text("Fade Transition"),
+          centerTitle: true,
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FadeTransition(
+              opacity: _animation,
+              child: Container(
+                height: 200,
+                width: 200,
+                decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                child: FlutterLogo(),
+              ),
+            ),
+            Divider(color: Colors.transparent, height: 20),
+            OutlineButton(
+              onPressed: () {
+                setState(() => _isForward = !_isForward);
+                Future.delayed(Duration(microseconds: 500), () {
+                  (_isForward) ? _controller.forward() : _controller.reverse();
+                });
+              },
+              borderSide: BorderSide(color: Colors.black),
+              child: Text("Change Fade Transition"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
