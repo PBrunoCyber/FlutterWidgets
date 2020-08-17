9f820f6f0d09c8f478938a407fe56e49a40e64aa
Paulo Bruno <paulo.bf111@gmail.com>
2020-08-17 16:46:02 -0300

    Transform added

---
+++ lib/pages/PaintingAndEffect/Transform/transform.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class TransformPage extends StatefulWidget {
+  @override
+  _TransformState createState() => _TransformState();
+}
+
+class _TransformState extends State<TransformPage>
+    with TickerProviderStateMixin {
+  //Variables
+  AnimationController _animationController;
+  List<Color> _color = [Colors.red, Colors.blue, Colors.indigo, Colors.green];
+  List<Animation> _rotate = List<Animation>(4),
+      _sizeWidth = List<Animation>(4),
+      _sizeHeight = List<Animation>(4);
+  Animation _curve;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(seconds: 2));
+    _curve = CurvedAnimation(parent: _animationController, curve: Curves.ease);
+    for (int i = 0; i < 4; i++) {
+      _sizeWidth[i] = Tween<double>(begin: 0, end: 120).animate(_curve);
+      _sizeHeight[i] = Tween<double>(begin: 0, end: 120).animate(_curve);
+      _rotate[i] = Tween<double>(begin: 0, end: 2.35).animate(_curve);
+    }
+    _animationController.addListener(() {
+      setState(() {});
+    });
+    _animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: CupertinoNavigationBar(
+          middle: Text("Testing"),
+          backgroundColor: Colors.grey.shade200,
+        ),
+        body: ListView.builder(
+          itemCount: 4,
+          itemBuilder: (context, index) {
+            return Stack(
+              alignment: Alignment.center,
+              children: [
+                Transform(
+                  alignment: Alignment.center,
+                  transform:
+                      Matrix4(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1)
+                        ..rotateZ(_rotate[index].value),
+                  child: Container(
+                    width: _sizeWidth[index].value,
+                    height: _sizeHeight[index].value,
+                    color: _color[index],
+                    child: CupertinoButton(
+                        child: Text(""), onPressed: () => print(index)),
+                  ),
+                ),
+              ],
+            );
+          },
+        ));
+  }
+}
