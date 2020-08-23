d904d47337dcb0807c5dc61f460d30016bbb3829
Paulo Bruno <paulo.bf111@gmail.com>
2020-08-23 12:41:19 -0300

    CustomSingleChildLayout added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:Learn_1/pages/LayoutWidget/container.dart';
+import 'package:Learn_1/pages/PaintingAndEffect/Transform/transform.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         appBarTheme: AppBarTheme(iconTheme: IconThemeData(color: Colors.white)),
       ),
       title: _title,
-      home: ContainerPage(),
+      home: TransformPage(),
     );
   }
 }
---
+++ lib/pages/LayoutWidget/customSingleChildLayout.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class CustomSingleChildLayoutPage extends StatefulWidget {
+  @override
+  _CustomSingleChildLayoutState createState() =>
+      _CustomSingleChildLayoutState();
+}
+
+class _CustomSingleChildLayoutState extends State<CustomSingleChildLayoutPage> {
+  @override
+  Widget build(BuildContext context) {
+    double _screenWidth = MediaQuery.of(context).size.width;
+    double _screenHeight = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: CupertinoNavigationBar(
+        middle: Text("Custom Single Child Layout"),
+        backgroundColor: Colors.grey.shade200,
+      ),
+      body: CustomSingleChildLayout(
+        delegate: Delegate(offset: Offset(_screenWidth / 8, _screenHeight / 2)),
+        child: GestureDetector(
+          onTap: () => print("Ok"),
+          child: Container(
+            constraints: BoxConstraints(maxHeight: 200, maxWidth: 300),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: Colors.grey[200],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Delegate extends SingleChildLayoutDelegate {
+  final Offset offset;
+  Delegate({@required this.offset});
+
+  @override
+  Offset getPositionForChild(Size size, Size childSize) {
+    return offset;
+  }
+
+  @override
+  bool shouldRelayout(SingleChildLayoutDelegate oldDelegate) {
+    return true;
+  }
+}
